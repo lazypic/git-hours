451cfa03bc348d9d066502c6f5a9ccab46ac5454
woong <woong@lazypic.org>
2019-03-29 20:32:16 +0900

    add stdout, stderr

--- githour.go
+++ githour.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -10,26 +11,32 @@ import (
 	)
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	cmd := exec.Command("git", "--no-pager", "log", "--reverse", "--date=iso", `--pretty=format:%ad %an %s`, `--after="2018-01-01 00:00:00 +0900"`, `--before="2019-12-31 23:59:59 +0900"`)
-	stdout, err := cmd.Output()
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if stderr.String() != "" {
+		fmt.Fprintf(os.Stderr, stderr.String())
+		os.Exit(1)
+	}
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, stderr.String())
+		os.Exit(1)
 	}
 	total, err := time.ParseDuration("1h")
 	if err != nil {
 		log.Fatal(err)
 	}
 	var before time.Time
-	if strings.HasPrefix(string(stdout), "fatal") {
-		fmt.Println(string(stdout))
-		os.Exit(1)
-	}
-	if string(stdout) == "" {
+	if stdout.String() == "" {
 		fmt.Println("0h")
 		os.Exit(0)
 	}
 
-	for n, l := range strings.Split(string(stdout), "\n") {
+	for n, l := range strings.Split(stdout.String(), "\n") {
 		lists := strings.Split(l, " ")
 		t := fmt.Sprintf("%sT%s%s:%s", lists[0], lists[1], lists[2][0:3], lists[2][3:5])
 		t1, err := time.Parse(time.RFC3339, t)
