8a891b276caf52d11bfec152109638a90dec023e
woong <woong@lazypic.org>
2019-03-29 20:53:13 +0900

    stderr 처리문 추가함.

--- githour.go
+++ githour.go
@@ -11,6 +11,9 @@ import (
 	)
 
 func main() {
+	// zone
+	// start
+	// end
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	cmd := exec.Command("git", "--no-pager", "log", "--reverse", "--date=iso", `--pretty=format:%ad %an %s`, `--after="2018-01-01 00:00:00 +0900"`, `--before="2019-12-31 23:59:59 +0900"`)
 	var stdout bytes.Buffer
@@ -28,37 +31,38 @@ func main() {
 	}
 	total, err := time.ParseDuration("1h")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "%v", err)
+		os.Exit(1)
 	}
-	var before time.Time
 	if stdout.String() == "" {
 		fmt.Println("0h")
 		os.Exit(0)
 	}
 
+	var before time.Time
 	for n, l := range strings.Split(stdout.String(), "\n") {
 		lists := strings.Split(l, " ")
-		t := fmt.Sprintf("%sT%s%s:%s", lists[0], lists[1], lists[2][0:3], lists[2][3:5])
-		t1, err := time.Parse(time.RFC3339, t)
+		iso2rfc3339 := fmt.Sprintf("%sT%s%s:%s", lists[0], lists[1], lists[2][0:3], lists[2][3:5])
+		t, err := time.Parse(time.RFC3339, iso2rfc3339)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Fprintf(os.Stderr, "%v", err)
 		}
 		if n == 0 {
-			before = t1
+			before = t
 			continue
 		}
-		elapsed := t1.Sub(before)
-		fmt.Println(elapsed)
+		elapsed := t.Sub(before)
+		fmt.Println("\t",elapsed)
 		h, err := time.ParseDuration("1h")
 		if err != nil {
-			fmt.Println(err)
+			fmt.Fprintf(os.Stderr, "%v", err)
 		}
 		if elapsed < h*2 {
 			total += elapsed
 		} else {
 			total += h
 		}
-		before = t1
+		before = t
 	}
 	fmt.Println(total)
 }
