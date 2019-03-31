c94ca908eab3d4cda057bf9b61bafb645b96f4dc
woong <woong@lazypic.org>
2019-03-31 15:12:56 +0900

    add flag

--- githour.go
+++ githour.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
-	"time"
 	"strings"
-	)
+	"time"
+)
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 func main() {
-	// zone
-	// start
-	// end
-	cmd := exec.Command("git", "--no-pager", "log", "--reverse", "--date=iso", `--pretty=format:%ad %an %s`, `--after="2018-01-01 00:00:00 +0900"`, `--before="2019-12-31 23:59:59 +0900"`)
+	startPtr := flag.String("start", "2018-01-01", "start date.")
+	endPtr := flag.String("end", "2019-12-31", "end date.")
+	zonePtr := flag.String("zone", "+0900", "zone offset time")
+	cmd := exec.Command(
+		"git",
+		"--no-pager",
+		"log",
+		"--reverse",
+		"--date=iso",
+		`--pretty=format:%ad %an %s`,
+		fmt.Sprintf(`--after="%s 00:00:00 %s"`, *startPtr, *zonePtr),
+		fmt.Sprintf(`--before="%s 23:59:59 %s"`, *endPtr, *zonePtr),
+	)
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &stdout
@@ -55,7 +65,7 @@ func main() {
 			continue
 		}
 		elapsed := t.Sub(before)
-		fmt.Println("\t",elapsed)
+		fmt.Println("\t", elapsed)
 		h, err := time.ParseDuration("1h")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%v\n", err)
