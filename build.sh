b96421e26dd52673b70cde2e50464f20f0d6cba6
kim hanwoong <khw7096@gmail.com>
2019-04-01 02:44:31 +0900

    add x86-64

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ GOOS=windows GOARCH=amd64 go build -o ./bin/windows/${APP}.exe git-hours.go
 GOOS=darwin GOARCH=amd64 go build -o ./bin/darwin/${APP} git-hours.go
 
 # Github Release에 업로드 하기위해 압축
-cd ./bin/linux/ && tar -zcvf ../${APP}_linux.tgz . && cd -
-cd ./bin/windows/ && tar -zcvf ../${APP}_windows.tgz . && cd -
-cd ./bin/darwin/ && tar -zcvf ../${APP}_darwin.tgz . && cd -
+cd ./bin/linux/ && tar -zcvf ../${APP}_linux_x86-64.tgz . && cd -
+cd ./bin/windows/ && tar -zcvf ../${APP}_windows_x86-64.tgz . && cd -
+cd ./bin/darwin/ && tar -zcvf ../${APP}_darwin_x86-64.tgz . && cd -
 
 # 삭제
 rm -rf ./bin/linux
